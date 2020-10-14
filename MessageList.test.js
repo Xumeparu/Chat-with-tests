f3d6b2452d249e7d41943ec13a7113ecbc71cb1c
Xumeparu <nyaluxey@gmail.com>
2020-10-14 15:54:06 +0700

    Update
    Update MessagesList

---
+++ MessageList.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import MessagesList from './MessagesList';
+
+describe('MessagesList', () => {
+    test('add message', () => {
+        const messages = [
+            {nick: "test", message: "test"}];
+        const component = shallow(<MessagesList messages={messages}/>);
+        expect(component.find('Message')).toHaveLength(1);
+    });
+});
