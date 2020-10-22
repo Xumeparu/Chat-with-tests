1b93c84144f4326b8dc88cfff289e8bc1c14e2d6
Xumeparu <nyaluxey@gmail.com>
2020-10-22 20:23:14 +0700

    Update
    Удалила лишний блок "test" в config\webpack.common.js
    Добавила иконку сайта.
    Подключила ее в index.html

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -49,13 +49,6 @@ module.exports = {
                 use: [
                     'style-loader',
                     { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
-                ],
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    'style-loader',
-                    { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
                     //{ loader: 'postcss-loader', options: { sourceMap: true } },
                     { loader: 'sass-loader', options: { sourceMap: true } },
                 ],
