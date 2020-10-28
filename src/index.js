9d0ec7a5d126e2086039f188929945b22e28c0bf
Xumeparu <nyaluxey@gmail.com>
2020-10-28 17:17:02 +0700

    Импорт history и react-route в index.js. Создана директория views с компонентами: LoginView.js, RegistrationView.js и ChatView.js. Содержимое App.js перемещено в ChatView.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
-    <App/>,
-    document.getElementById("section")
+    <Router history={history}>
+        <App />
+    </Router>,
+    document.getElementById('root')
 );
---
+++ src/views/ChatView.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import Form from './components/Form';
+import MessagesList from "./components/MessagesList";
+import Index from "./components/PainCat";
+
+const URL = 'http://localhost:3000';
+
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            serverMessages:[]
+        };
+
+        setInterval(this.getMessages.bind(this), 1000);
+    }
+
+    sendMessage(newMessage){
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', URL);
+        xhr.send(JSON.stringify(newMessage));
+
+        xhr.onload = () => {
+            if (xhr.status !== 200) {
+                console.error('Ошибка!');
+            } else {
+                this.parseMessages(xhr.response);
+            }
+        };
+
+        xhr.onerror = function () {
+            console.log('Запрос не удался');
+        };
+    }
+
+    getMessages(){
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', URL);
+        xhr.send();
+        xhr.onload = () => {
+            if (xhr.status !== 200) {
+                console.error('Ошибка!');
+            } else {
+                this.parseMessages(xhr.response);
+            }
+        };
+    }
+
+    parseMessages(response){
+        const newServerMessages = JSON.parse(response);
+        this.setState({
+            serverMessages: newServerMessages
+        });
+    }
+
+    render(){
+        const {serverMessages} = this.state;
+
+        return <>
+            <h1>Chat</h1>
+            <Index/>
+            <Form sendMessage={(newMessage) => this.sendMessage(newMessage)}/>
+            <MessagesList messages={serverMessages}/>
+        </>
+    }
+}
+
+export default App;
