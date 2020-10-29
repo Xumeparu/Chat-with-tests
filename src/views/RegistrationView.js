af37193c0b5da07c37b3c3db882937af2f14be00
Xumeparu <nyaluxey@gmail.com>
2020-10-29 19:45:45 +0700

    Добавлено содержание в RegistrationView.js.

--- src/views/RegistrationView.js
+++ src/views/RegistrationView.js
@@ -1,16 +1,55 @@
 import React from 'react';
 import axios from 'axios';
 
-const instance = axios.create({
-    baseURL: 'https://some-domain.com/api/'
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:3001'
 });
-instance.get(URL)
 
 export default class RegistrationView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nickname: "",
+            password: ""
+        };
+    }
+
+    handleSubmit(e) {
+        axiosInstance.post('/user', {
+            nickname: this.state.nickname,
+            password: this.state.password
+        });
+        e.preventDefault();
+    }
+
     render() {
+        const { nickname, password } = this.state;
         return (
             <>
                 <h1>Registration</h1>
+                <form onSubmit={(e) => this.handleSubmit(e)}>
+                    <div>
+                        <label>
+                            Nickname:&nbsp;
+                            <input
+                                type="text"
+                                value={nickname}
+                                onChange={e => this.setState({ nickname: e.target.value})}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Password:&nbsp;
+                            <input
+                                type="text"
+                                value={password}
+                                onChange={e => this.setState({ password: e.target.value})}
+                            />
+                        </label>
+                    </div>
+                    <button type="submit">Create user</button>
+                </form>
             </>
         );
     }
