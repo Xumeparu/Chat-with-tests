388a8b6dbc6b270494fd5cef99f35e1374c1972d
Xumeparu <nyaluxey@gmail.com>
2020-11-08 15:53:05 +0700

    Chat.js has been created. Added its content

---
+++ src/components/Chat.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Chat extends React.Component {
+    innerClickHandle(e) {
+        e.preventDefault();
+        this.props.clickHandle(this.props.id);
+    }
+
+    render() {
+        const { title } =this.props;
+        return (
+            <li>
+                <a href="/" onClick={e => this.innerClickHandle(e)}>
+                    {title}
+                </a>
+            </li>
+        );
+    }
+}
+
+Chat.propTypes = {
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    clickHandle: PropTypes.func.isRequired
+};
+
+export default Chat;
