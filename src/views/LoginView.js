c3962f4eb743f0cd328b43f038e6eb0d17a712f1
Xumeparu <nyaluxey@gmail.com>
2020-11-16 01:10:12 +0700

    Updated LoginView.js content: added Formik validation and formatting code

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Formik } from 'formik';
 import PropTypes from 'prop-types';
 import apiServices from '../apiServices';
 import styles from './LoginView.module.css';
@@ -7,63 +8,100 @@ export default class LoginView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            nickname: '',
-            password: '',
             successMessage: '',
             errorMessage: ''
         };
     }
 
-    handleSubmit(e) {
-        const { nickname, password } = this.state;
-        e.preventDefault();
+    handleSubmit(values) {
         this.setState({
-            successMessage: '',
-            errorMessage: ''
+            successMessage: null,
+            errorMessage: null
         });
-        apiServices.auth
-            .login({ nickname, password })
-            .then(() => this.setState({ successMessage: 'User registered successfully' }))
-            .then(() => setTimeout(() => this.props.history.push('/profile'), 2000))
+
+        return apiServices.auth
+            .login(values)
+            .then(() => {
+                this.setState({ successMessage: 'User registered successfully' });
+                setTimeout(() => this.props.history.push('/profile'), 2000);
+            })
             .catch((error) =>
                 this.setState({ errorMessage: 'Error! ' + error.response.data.error })
             );
     }
 
     render() {
-        const { nickname, password, successMessage, errorMessage } = this.state;
         return (
             <>
                 <h1>Authentication</h1>
-                <div className={styles.errorMessage}>{errorMessage}</div>
-                <div className={styles.successMessage}>{successMessage}</div>
-                <form onSubmit={(e) => this.handleSubmit(e)}>
-                    <div>
-                        <label>
-                            Nickname:&nbsp;
-                            <input
-                                type="text"
-                                className="nick-pass"
-                                value={nickname}
-                                onChange={(e) => this.setState({ nickname: e.target.value })}
-                            />
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            Password:&nbsp;
-                            <input
-                                type="password"
-                                className="nick-pass"
-                                value={password}
-                                onChange={(e) => this.setState({ password: e.target.value })}
-                            />
-                        </label>
-                    </div>
-                    <button type="submit" className="button">
-                        Go!
-                    </button>
-                </form>
+                <div className={styles.successMessage}>{this.state.successMessage}</div>
+                <Formik
+                    initialValues={{
+                        nickname: '',
+                        password: ''
+                    }}
+                    validate={(values) => {
+                        const errors = {};
+                        if (values.nickname.length === 0) {
+                            errors.nickname = 'Enter your nickname, please';
+                        }
+                        if (values.password.length === 0) {
+                            errors.password = 'Enter your password, please';
+                        }
+                        return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                        this.handleSubmit(values).then(() => setSubmitting(false));
+                    }}
+                >
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting
+                    }) => (
+                        <form onSubmit={handleSubmit}>
+                            {errors.nickname && touched.nickname && (
+                                <div className={styles.errorMessage}>{errors.nickname}</div>
+                            )}
+                            <div>
+                                <label>
+                                    Nickname:&nbsp;
+                                    <input
+                                        type="text"
+                                        name="nickname"
+                                        className="nick-pass"
+                                        value={values.nickname}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                    />
+                                </label>
+                            </div>
+                            {errors.password && touched.password && (
+                                <div className={styles.errorMessage}>{errors.password}</div>
+                            )}
+                            <div>
+                                <label>
+                                    Password:&nbsp;
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        className="nick-pass"
+                                        value={values.password}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                    />
+                                </label>
+                            </div>
+                            <button type="submit" className="button" disabled={isSubmitting}>
+                                Go!
+                            </button>
+                        </form>
+                    )}
+                </Formik>
             </>
         );
     }
