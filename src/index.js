9c7702a92791d69889284022c5a2d463feae9687
Xumeparu <nyaluxey@gmail.com>
2020-11-22 21:23:37 +0700

    Updated index.js and package.json content: history has been deleted

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-
-const history = createBrowserHistory();
 
 ReactDOM.render(
-    <Router history={history}>
+    <Router >
         <App />
     </Router>,
     document.getElementById('container')
