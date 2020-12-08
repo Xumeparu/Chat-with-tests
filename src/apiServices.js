09ee2d90f6fcfc5369177df1ab2a028a7aa16904
Xumeparu <nyaluxey@gmail.com>
2020-12-08 19:11:34 +0700

    Changed baseURL in apiServices.js. Connecting to a new server

--- src/apiServices.js
+++ src/apiServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://shielded-anchorage-94550.herokuapp.com',
     withCredentials: true
 });
 
